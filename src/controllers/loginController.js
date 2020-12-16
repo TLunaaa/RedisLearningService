be7e4fd456a39997da669d914699a7c23f681891
Tomas Luna <tomas_luna1996@hotmail.com>
2020-12-16 09:39:13 -0300

    logs to watch req body at login controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,6 +3,7 @@ var loginService = require('../services/loginService'),
 
 exports.register = async function(req,res,next){
     try{
+        console.log(req.body);
         await loginService.register(req.body);
         res.status(200).end();
     }catch(err){
@@ -17,6 +18,7 @@ exports.register = async function(req,res,next){
 
 exports.login = async function(req,res,next){
     try{
+        console.log(req.body);
         let userData = await loginService.login(req.body);
         res.status(200).json(userData);
     }catch(err){
